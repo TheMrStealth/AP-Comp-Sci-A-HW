cf935678434ac2bb60b15594df8429ff5f3ec802
robert.gillette <rjgillette2007@gmail.com>
2026-03-04 11:03:50 -0500

    stuff

--- src/HW18_Slide438.java
+++ src/HW18_Slide438.java
@@ -7,7 +7,8 @@ import java.util.*;
 import java.io.*;
 
 public class HW18_Slide438 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File inpt = new File("input.txt");
+        Scanner in = new Scanner(inpt);
     }
 }
