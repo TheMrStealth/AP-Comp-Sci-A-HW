41f44314d386b743c34198cf990a5501b11baef4
robert.gillette <rjgillette2007@gmail.com>
2026-03-11 11:04:43 -0400

    stuff

--- src/HW18_Slide438/HW18Q1.java
+++ src/HW18_Slide438/HW18Q1.java
@@ -13,5 +13,14 @@ public class HW18Q1 {
         for (int i = 0; i < students.length; i++) {
             students[i] = data.nextLine().split(",");
         }
+        int mindex = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (Integer.parseInt(students[i][2]) < Integer.parseInt(students[mindex][2])) {
+                mindex = i;
+            }
+        }
+        for (String s : students[mindex]) {
+            System.out.print(s+" ");
+        }
     }
 }
