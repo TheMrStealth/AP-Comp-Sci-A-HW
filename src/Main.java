069952e4d5eaf2b9698d3f9e50f881624ab3a0a0
robert.gillette <rjgillette2007@gmail.com>
2026-03-24 11:05:04 -0400

    stuff

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,21 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1,2,6,1,54,6,2,56,1,3));
-        System.out.println(a);
-        for (int i = 0; i < a.size()-1; i++) {
-            if (a.get(i)>a.get(i+1)) {
-                a.remove(i+1);
-                i--;
-            }
-        }
+        ArrayList<ArrayList<Integer>> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        b.add(1);
+        b.add(1);
+        b.add(1);
+        b.add(1);
+        b.add(1);
+        ArrayList<Integer> c = new ArrayList<>();
+        c.add(2);
+        c.add(2);
+        c.add(2);
+        c.add(2);
+        c.add(2);
+        a.add(b);
+        a.add(c);
         System.out.println(a);
     }
 }
